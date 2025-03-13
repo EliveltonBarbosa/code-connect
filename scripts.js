b24e847422aa561e4930ccfba251a4d26759cbb3
Barbosa <barbosa.elivelton.dev@gmail.com>
2025-03-13 08:36:33 -0300

    add tag input functionality with dynamic tag creation and removal

--- scripts.js
+++ scripts.js
@@ -6,10 +6,10 @@ uploadBtn.addEventListener('click', () => {
 });
 
 function readFileContent(file) {
-    return new Promise((resolve,  reject) => {
+    return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.onload = () => {
-            resolve({url: reader.result, name: file.name});
+            resolve({ url: reader.result, name: file.name });
         };
         reader.onerror = () => {
             reject(`Erro ao ler o arquivo ${file.name}`);
@@ -18,22 +18,43 @@ function readFileContent(file) {
     });
 }
 
+const mainImagem = document.querySelector('.main-imagem');
+const nomeImagem = document.querySelector('.container-imagem-nome p');
+
 inputUpload.addEventListener('change', async (event) => {
     const file = event.target.files[0];
     if (file) {
-        await readFileContent(file);
+        try {
+            const fileContent = await readFileContent(file);
+            mainImagem.src = fileContent.url;
+            nomeImagem.textContent = fileContent.name;
+        }
+        catch (error) {
+            console.error(`container-imagem-nome:${error}`);
+            alert("Erro na leitura do arquivo");
+        }
     }
 });
 
-// document.getElementById('imageInput').addEventListener('change', function(event) {
-//     const file = event.target.files[0]; // Pegando o arquivo selecionado pelo usuário
-//     if (file) {
-//         const reader = new FileReader(); // Criando uma instância do FileReader
-//         reader.onload = function(e) {
-//             const preview = document.getElementById('preview');
-//             preview.src = e.target.result; // Atribuindo o resultado da leitura como fonte da imagem de pré-visualização
-//             preview.style.display = 'block'; // Tornando a pré-visualização visível
-//         };
-//         reader.readAsDataURL(file); // Lendo o arquivo como um Data URL
-//     }
-// });
+const inputTags = document.getElementById('input-tags');
+const tagList = document.getElementById('lista-tags');
+
+inputTags.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        const tag = inputTags.value.trim();
+        if (tag) {
+            const tagElement = document.createElement('li');
+            tagElement.innerHTML = `<p>${tag}</p> <img src="./img/close-black.svg" class="remove-tag">`;
+            tagList.appendChild(tagElement);
+            inputTags.value = '';
+        }
+    }
+});
+
+tagList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('remove-tag')) {
+        const toRemove = event.target.parentElement;
+        tagList.removeChild(toRemove);
+    }
+});
